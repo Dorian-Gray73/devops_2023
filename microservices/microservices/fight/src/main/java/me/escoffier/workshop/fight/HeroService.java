ff1298ab6f681d8ee0b9ee7c22a2dc2a547755e3
DorianGray <doriansimon73@gmail.com>
2023-01-22 14:19:16 +0100

    feat: :sparkles: health
    ajout d'une dépendance quarkus permettant de vérifier la santé de nos pods supes-hero && sueps-vilain

--- microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/HeroService.java
+++ microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/HeroService.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @RegisterRestClient(configKey = "supes-hero")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(MediaType.APPLICATION_JSON)
 public interface HeroService {
 
     @Path("/hero")
--- microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/VillainService.java
+++ microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/VillainService.java
@@ -8,7 +8,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @RegisterRestClient(configKey = "supes-vilain")
-@Produces(MediaType.TEXT_PLAIN)
+@Produces(MediaType.APPLICATION_JSON)
 public interface VillainService {
 
     @Path("/villain")
