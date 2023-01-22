3e32ab5bb3d0e4a1855042605d0c0e37df44e2e1
DorianGray <doriansimon73@gmail.com>
2023-01-22 08:58:03 +0100

    refactor: :sparkles: modification artifactId

--- microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/HeroService.java
+++ microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/HeroService.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@RegisterRestClient(configKey = "hero-service")
+@RegisterRestClient(configKey = "supes-hero")
 @Produces(MediaType.TEXT_PLAIN)
 public interface HeroService {
 
--- microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/VillainService.java
+++ microservices/microservices/fight/src/main/java/me/escoffier/workshop/fight/VillainService.java
@@ -7,7 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@RegisterRestClient(configKey = "villain-service")
+@RegisterRestClient(configKey = "supes-vilain")
 @Produces(MediaType.TEXT_PLAIN)
 public interface VillainService {
 
